e3c7c1a61044d0fda06433734c2aaf297963fc33
AbdulRasheed02 <83546777+AbdulRasheed02@users.noreply.github.com>
2022-01-30 00:05:24 +0530

    Updated:User Store

--- src/mobx/user_store.js
+++ src/mobx/user_store.js
@@ -2,20 +2,32 @@ import {action, makeObservable, observable, computed} from 'mobx';
 
 class UserStore {
   state = {
+    userName: '',
     userType: null,
   };
+
+  setUserName = val => {
+    this.state.userName = val;
+  };
+
+  get getUserName() {
+    return this.state.userName;
+  }
 
   setUserType = val => {
     this.state.userType = val;
   };
 
   get getUserType() {
-    return this.state.getUserType;
+    return this.state.userType;
   }
 
   constructor() {
     makeObservable(this, {
       state: observable,
+
+      setUserName: action,
+      getUserName: computed,
 
       setUserType: action,
       getUserType: computed,
