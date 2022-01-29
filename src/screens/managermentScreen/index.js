5695a4cc70037f3a5921f8ff0a962e6babf170f9
AbdulRasheed02 <83546777+AbdulRasheed02@users.noreply.github.com>
2022-01-29 19:32:21 +0530

    Added:Src Folder

---
+++ src/screens/managermentScreen/index.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const ManagementScreen = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default ManagementScreen;
